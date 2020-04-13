c77e3004e5d0ac7109d205323bd4f72d42435b7b
Joel Eager <git@joeleager.me>
2020-04-13 18:45:55 -0400

    Completed the F# implementation

--- FSharpImplementation/Library.fs
+++ FSharpImplementation/Library.fs
@@ -8,7 +8,7 @@ module SeparatingAxisTheorem =
         printfn "%A" values
         values
 
-    let max (values: array<int>) =
+    let maxArray (values: array<int>) =
         let mutable maxValue = values.[0]
 
         for value in values do
@@ -16,7 +16,7 @@ module SeparatingAxisTheorem =
 
         maxValue
 
-    let min (values: array<int>) =
+    let minArray (values: array<int>) =
         let mutable minValue = values.[0]
 
         for value in values do
@@ -28,20 +28,37 @@ module SeparatingAxisTheorem =
         point2.[0] - point1.[0], point2.[1] - point1.[1]
 
     let polyToEdges (poly: array<array<int>>) =
-        [ for i in 0 .. poly.Length - 1 do
-            yield edgeVector poly.[0] poly.[(i + 1) % poly.Length] ]
+        [| for i in 0 .. poly.Length - 1 do
+            yield edgeVector poly.[0] poly.[(i + 1) % poly.Length] |]
 
-    let orthogonal (vector: array<int>) =
-        vector.[1], -vector.[0]
+    let orthogonal vector =
+        snd vector, -fst vector
 
-    let dotProduct (vector1: array<int>) (vector2: array<int>) =
-        vector1.[0] * vector2.[0] + vector1.[1] * vector2.[1]
+    let dotProduct vector1 vector2 =
+        fst vector1 * fst vector2 + snd vector1 * snd vector2
 
-    let project poly axis =
-        let dots = [for point in poly do yield dotProduct point axis]
-        min dots, max dots
+    let project (poly: array<array<int>>) axis =
+        let dots =
+            [| for point in poly do
+                yield dotProduct (point.[0], point.[1]) axis |]
+        minArray dots, maxArray dots
+
+    let overlap (projection1: int * int) (projection2: int * int) =
+        min (fst projection1) (snd projection1) <= max (fst projection2) (snd projection2)
+        && min (fst projection2) (snd projection2) <= max (fst projection1) (snd projection1)
 
     let hasCollied (poly1: array<array<int>>) (poly2: array<array<int>>) =
-        printfn "%A" poly1
-        printfn "%A" poly2
-        true
+        let edges =
+            Array.concat
+                [ polyToEdges poly1
+                  polyToEdges poly2 ]
+        let axes =
+            [| for edge in edges do
+                yield orthogonal edge |]
+
+        let mutable overlapping = true
+
+        for axis in axes do
+            if overlapping then overlapping <- overlap (project poly1 axis) (project poly2 axis)
+
+        overlapping
